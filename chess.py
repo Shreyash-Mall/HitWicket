6be05938c0523220c915b9d69789bf8bf3314f9c
Shreyash Mall <67573963+Shreyash-Mall@users.noreply.github.com>
2022-09-10 19:26:03 +0530

    Create chess.py

---
+++ chess.py
@@ -0,0 +1,156 @@
+# Defining the Functions for the H1, H2 and H3 Moves  
+def moveh1a(k3,c):
+    l6=c.split(':')
+    a=l6[0]
+    b=l6[1]
+    for i in range(0,5):
+        for j in range(0,5):
+            l=k3[i][j].split('-')
+            if(l[0]=='A'):
+                if(l[1]==a):
+                    if(l[1]=='F'):
+                        k3[i-2][j]=k3[i][j]
+                        k3[i][j]='-'
+                        k[i-1][j]='-'
+                    else:
+                        k3[i+2][j]=k3[i][j]
+                        k3[i][j]='-'
+                        k3[i+1][j]='-'
+    return k3
+            
+  
+def moveh1b(k3,c):
+    
+    l6=c.split(':')
+    a=l6[0]
+    b=l6[1]
+    for i in range(0,5):
+        for j in range(0,5):
+            l=k3[i][j].split('-')
+            if(l[0]=='B'):
+                if(l[1]==a):
+                    if(l[1]=='F'):
+                        k3[i+2][j]=k3[i][j]
+                        k3[i+1][j]='-'
+                        k3[i][j]='-'
+                    else:
+                        k3[i-2][j]=k3[i][j]
+                        k3[i-1][j]='-'
+                        k3[i][j]='-'
+    return k3 
+
+def matrix(m,li,k,c):
+    for i in range(0,5):
+        for j in range(0,5):
+            if(i==m):
+                if c%2==0:
+                    k1='A-'
+                else:
+                    k1='B-'
+                k[i][j]=k1+li[j]
+            else:
+                k[i][j]='-'
+    
+    return k 
+
+
+def printmat(v):
+    print("Printed Matrix: ")
+    for i in range(0,5):
+        for j in range(0,5):
+            print(v[i][j],end='')
+        print('')
+        
+def s(a):
+    l=a.split(",")
+    li=[]
+    
+    for i in l:
+        li.append(i)
+    return li
+
+
+def movea(k3,c):
+
+    l6=c.split(':')
+    a=l6[0]
+    b=l6[1]
+    for i in range(0,5):
+        for j in range(0,5):
+            l=k3[i][j].split('-')
+            if(l[0]=='A'):
+                if(l[1]==a):
+                    if(l[1]=='F'):
+                        k3[i-1][j]=k3[i][j]
+                        k3[i][j]='-'
+                    else:
+                        k3[i+1][j]=k3[i][j]
+                        k3[i][j]='-'
+    return k3
+                  
+def moveb(k3,c):
+    
+    l6=c.split(':')
+    a=l6[0]
+    b=l6[1]
+    for i in range(0,5):
+        for j in range(0,5):
+            l=k3[i][j].split('-')
+            if(l[0]=='B'):
+                if(l[1]==a):
+                    if(l[1]=='F'):
+                        k3[i+1][j]=k3[i][j]
+                        k3[i][j]='-'
+                    else:
+                        k3[i-1][j]=k3[i][j]
+                        k3[i][j]='-'
+    return k3 
+
+#Driver Code  
+a=input("Player1 enter input: ")
+c=0
+li=s(a)
+k3=[[]]
+d='-'
+for i in range(0,5):
+    k3.append()
+    for j in range(0,5):
+        k3[i][j]='-'
+printmat(k3)
+k3=matrix(4,li,k3,c)
+c=c+1
+printmat(k3)
+b=input("Player2 enter input: ")
+li=s(b)
+k3=matrix(0,li,k3,c)
+printmat(k3)
+while(1):
+    c=input("Enter input for player 1: ")
+    l9=c.split(':')
+    if 'P' in l9[0]:
+        
+        k3=movea(k3,c)
+    else:
+        if 'H1' in l9[0]:
+            k3=moveh1a(k3,c)
+        else:
+            k3=moveh2a(k3,c)
+    c=input("Enter input for player 2")
+    l9=c.split(':')
+    if 'P' in l9[0]:
+        
+        k3=moveb(k3,c)
+    else:
+        if 'H1' in l9[0]:
+            k3=moveh1b(k3,c)
+        else:
+            k3=moveh2b(k3,c)
+    ans=input("Do you want to continue y/n")
+    if ans=='y':
+        break
+
+
+# Could Not Complete Further due to time restrictions
+        
+        
+        
